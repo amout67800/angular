59a39db138cd6a9be94bc03e2bf3508b51587a79
amoutier <amout67800@hotmail.com>
2019-03-30 22:21:46 +0100

    implement home content

--- src/app/common/components/home/home.component.ts
+++ src/app/common/components/home/home.component.ts
@@ -7,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  advices = [
+    { id: 1,  label: 'What souhld you put in your backpack ?' },
+    { id: 2,  label: 'Travel pharmacy' },
+    { id: 3,  label: 'Procedures before depature' },
+    { id: 4,  label: 'How to organize a long trip ?' },
+    { id: 5,  label: 'Travel accessories' },
+    { id: 6,  label: 'Children check list' },
+    { id: 7,  label: 'How to prepare your backpack' },
+    { id: 8,  label: 'Traveling with baby/children' },
+    { id: 9,  label: 'The best backpacks' }
+  ]
   constructor() { }
 
   ngOnInit() {
